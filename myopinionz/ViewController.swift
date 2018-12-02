19e1ca5d3de0ab9e9bb8dcf019c6bdae4daa44f0
Prudhvi Monikuntla <prudhvimonikuntla@metova.com>
2018-12-02 15:38:05 -0600

    Initial Commit

---
+++ myopinionz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myopinionz
+//
+//  Created by Prudhvi Monikuntla on 12/2/18.
+//  Copyright © 2018 Prudhvi Monikuntla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
