d14361facf7698e31d503f9f0dfd3d50a5230a09
Prudhvi Monikuntla <prudhvimonikuntla@gmail.com>
2018-12-09 00:54:17 -0600

    -added OptionsToLoginVC and LoginVC

---
+++ myopinionz/ui/view/login/OptionsToLoginViewController.swift
@@ -0,0 +1,62 @@
+//
+//  OptionsToLoginViewController.swift
+//  myopinionz
+//
+//  Created by Prudhvi Monikuntla on 12/9/18.
+//  Copyright © 2018 Prudhvi Monikuntla. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
+class OptionsToLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
